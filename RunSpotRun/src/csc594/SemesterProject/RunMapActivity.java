bbfa1a7246cc423e002f5d274a6b8b29fde82bcf
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-03-30 15:20:49 +0000

    Seperated into 2 activities. MainActivity - the starting activity RunMapActivity - launches the MapActivity

---
+++ RunSpotRun/src/csc594/SemesterProject/RunMapActivity.java
@@ -0,0 +1,39 @@
+package csc594.SemesterProject;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapView.LayoutParams;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class RunMapActivity extends MapActivity 
+{
+	MapView mapView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapmain);
+        
+        mapView = (MapView) findViewById(R.id.mapView);
+        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
+        View zoomView = mapView.getZoomControls(); 
+
+        zoomLayout.addView(zoomView, 
+            new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT)); 
+        mapView.displayZoomControls(true);
+    }
+ 
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+	
+}
