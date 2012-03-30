d061efd375374541ae83e634ec29d59b6931bd04
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-03-30 15:20:49 +0000

    Seperated into 2 activities. MainActivity - the starting activity RunMapActivity - launches the MapActivity

---
+++ trunk/RunSpotRun/src/csc594/SemesterProject/MainActivity.java
@@ -0,0 +1,36 @@
+package csc594.SemesterProject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);        
+    }
+    
+    public void doGoToMap(View view)
+    {    	
+    	Intent launchGetRank = new Intent(this, RunMapActivity.class);
+		startActivityForResult(launchGetRank, 0);
+    }
+    
+	@Override
+	protected void onActivityResult(int requestCode,
+							int resultCode, Intent data) 
+	{
+		if (resultCode == RESULT_OK) 
+		{
+			//fromGetRank = data.getExtras().getString("returnStr");
+			//etRank.setText(fromGetRank);
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+}
