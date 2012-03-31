1448a8442f691711b9bbf6e753686631ca3f533d
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-03-31 00:58:37 +0000

    Stable Demo for group.

--- trunk/RunSpotRun/src/csc594/SemesterProject/MainActivity.java
+++ trunk/RunSpotRun/src/csc594/SemesterProject/MainActivity.java
@@ -1,15 +1,12 @@
 package csc594.SemesterProject;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import csc594.SemesterProject.MyGeoPoint.MyPointType;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import csc594.SemesterProject.MyGeoPoint.MyPointType;
 
 public class MainActivity extends Activity {
 
@@ -24,18 +21,19 @@ public class MainActivity extends Activity {
     public void doGoToMap(View view)
     {    	
     	Intent launchMap = new Intent(this, RunMapActivity.class);
-    	
-    	ArrayList<ParcMyGeoPoint> route = new ArrayList<ParcMyGeoPoint>();
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39312718,	-84281230, MyPointType.Start)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39313623,	-84282732)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39313847,	-84283859)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39314694,	-84284137)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39315831,	-84281509)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39318919,	-84279041)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39321500,	-84273033)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39319724,	-84271874)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39314188,	-84277571)));
-		route.add(new ParcMyGeoPoint(new MyGeoPoint(39312594,	-84280490, MyPointType.Stop)));
+		
+    	//Debug test hardcoded route
+    	ArrayList<MyGeoPoint> route = new ArrayList<MyGeoPoint>();
+		route.add(new MyGeoPoint(39312718,	-84281230, MyPointType.Start));
+		route.add(new MyGeoPoint(39313623,	-84282732));
+		route.add(new MyGeoPoint(39313847,	-84283859));
+		route.add(new MyGeoPoint(39314694,	-84284137));
+		route.add(new MyGeoPoint(39315831,	-84281509));
+		route.add(new MyGeoPoint(39318919,	-84279041));
+		route.add(new MyGeoPoint(39321500,	-84273033));
+		route.add(new MyGeoPoint(39319724,	-84271874));
+		route.add(new MyGeoPoint(39314188,	-84277571));
+		route.add(new MyGeoPoint(39312594,	-84280490, MyPointType.Stop));
 		
     	launchMap.putExtra("Route", route);
 		startActivity(launchMap);
@@ -47,8 +45,7 @@ public class MainActivity extends Activity {
 	{
 		if (resultCode == RESULT_OK) 
 		{
-			//fromGetRank = data.getExtras().getString("returnStr");
-			//etRank.setText(fromGetRank);
+			//do nothing
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
--- trunk/RunSpotRun/src/csc594/SemesterProject/MyGeoPoint.java
+++ trunk/RunSpotRun/src/csc594/SemesterProject/MyGeoPoint.java
@@ -1,9 +1,12 @@
 package csc594.SemesterProject;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.maps.GeoPoint;
 
 /** Class to hold our location information */
-public class MyGeoPoint 
+public class MyGeoPoint implements Parcelable
 {
 	enum MyPointType{ None, Normal, Start, Stop}
 	
@@ -19,50 +22,24 @@ public class MyGeoPoint
 	//name of point
 	private String mName;
 		
-	public MyGeoPoint(int latitude, int longitude, String time, String dist, String name, String type) 
+	public MyGeoPoint(int latitude, int longitude, String time, String dist, String name, MyPointType type) 
 	{
 		this.mName = name;
 		this.mTime = time;
 		this.mDistance = dist;
 		this.mType = MyPointType.None;
-		mPoint = new GeoPoint(latitude,longitude);
-				
-		if(type.equals(MyPointType.Normal.toString()))
-		{ this.mType = MyPointType.Normal;}
-		else if(type.equals(MyPointType.Start.toString()))
-		{ this.mType = MyPointType.Start;}
-		else if(type.equals(MyPointType.Stop.toString()))
-		{ this.mType = MyPointType.Stop;}
-		else//DEFAULT: if(type.equals(MyPointType.None.toString()))
-		{ this.mType = MyPointType.None;}
+		this.mPoint = new GeoPoint(latitude,longitude);
+		this.mType = type;
 	}
 	
 	public MyGeoPoint(int latitude, int longitude, MyPointType type) 
 	{
-		Double lat = (latitude/1E6);
-		Double lon = (longitude/1E6);
-		this.mName = lat.toString() + "\n" + lon.toString();
-		this.mTime = "12-1-1 12:12:22";
-		this.mDistance = "1.1";
-		this.mType = type;
-		mPoint = new GeoPoint(latitude,longitude);
+		this(latitude,longitude, "12-1-1 12:12:22","1.1", "",type);
 	}
 	
-	public MyGeoPoint(int latitude, int longitude) 
+	public MyGeoPoint(int latitude, int longitude)
 	{
-		Double lat = (latitude/1E6);
-		Double lon = (longitude/1E6);
-		this.mName = lat.toString() + "\n" + lon.toString();
-		this.mTime = "12-1-1 12:12:22";
-		this.mDistance = "1.1";
-		this.mType = MyPointType.Normal;
-		mPoint = new GeoPoint(latitude,longitude);
-	}
-	
-	public MyGeoPoint(String name,double latitude, double longitude) 
-	{
-		this.mName = name;
-		mPoint = new GeoPoint((int)(latitude*1e6),(int)(longitude*1e6));
+		this(latitude,longitude, "12-1-1 12:12:22","1.1", "", MyPointType.Normal);
 	}
 
 	public GeoPoint getPoint() {
@@ -101,4 +78,54 @@ public class MyGeoPoint
 	public String getName() {
 		return mName;
 	}
+
+//BELOW is Parcel Implementation Code 
+// Basically the way we can pass a List of My GeoPoints into the Map Activity easily
+	public int describeContents() {		
+		return 0;
+	}
+
+    public void writeToParcel(Parcel out, int flags) {
+    	//GeoPoint
+        out.writeInt(mPoint.getLatitudeE6());
+        out.writeInt(mPoint.getLongitudeE6());
+        //Time
+        out.writeString(mTime);        
+        //Distance
+        out.writeString(mDistance);
+        //Name
+        out.writeString(mName);
+        //Type
+        out.writeString(mType.toString());
+    }
+
+    public static final Parcelable.Creator<MyGeoPoint> CREATOR
+            = new Parcelable.Creator<MyGeoPoint>() {
+        public MyGeoPoint createFromParcel(Parcel in) {
+            return new MyGeoPoint(in);
+        }
+
+        public MyGeoPoint[] newArray(int size) {
+            return new MyGeoPoint[size];
+        }
+    };
+
+    private MyGeoPoint(Parcel in) {
+    	//MUST be same order as write above!!!
+        int lat = in.readInt();
+        int lon = in.readInt();
+        mPoint = new GeoPoint(lat,lon);
+        mTime = in.readString();
+        mDistance = in.readString();
+        mName = in.readString();
+        String typeAsString = in.readString();
+		if(typeAsString.equals(MyPointType.Normal.toString()))
+		{ this.mType = MyPointType.Normal;}
+		else if(typeAsString.equals(MyPointType.Start.toString()))
+		{ this.mType = MyPointType.Start;}
+		else if(typeAsString.equals(MyPointType.Stop.toString()))
+		{ this.mType = MyPointType.Stop;}
+		else//DEFAULT: if(type.equals(MyPointType.None.toString()))
+		{ this.mType = MyPointType.None;}
+    }
 }
