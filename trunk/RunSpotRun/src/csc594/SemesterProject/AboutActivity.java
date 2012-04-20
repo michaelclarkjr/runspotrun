21887ace2fa083028a8537f5d810b2b67faca9cf
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-04-20 20:43:12 +0000

    Added About Form to menu

---
+++ trunk/RunSpotRun/src/csc594/SemesterProject/AboutActivity.java
@@ -0,0 +1,15 @@
+package csc594.SemesterProject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);	
+	    setContentView(R.layout.about);    
+	}
+
+}
