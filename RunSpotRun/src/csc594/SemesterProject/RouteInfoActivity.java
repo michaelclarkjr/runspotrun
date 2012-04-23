2653bddfe5b974414b90aad49e5a872d4830232d
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-04-23 01:22:49 +0000

    Updated to read from DB

--- RunSpotRun/src/csc594/SemesterProject/RouteInfoActivity.java
+++ RunSpotRun/src/csc594/SemesterProject/RouteInfoActivity.java
@@ -1,9 +1,9 @@
 package csc594.SemesterProject;
 
 import java.util.ArrayList;
-
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RouteInfoActivity extends Activity {
@@ -18,16 +20,34 @@ public class RouteInfoActivity extends Activity {
 	public static final int MENU_SETTINGS = Menu.FIRST+1;
 	public static final int MENU_ABOUT = Menu.FIRST+2;
 	
+	private int routeKeyDB;
+	private RouteItem route;
+	
+	private TextView tvDate, tvTime, tvDistance, tvSpeed;
+	
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
         setContentView(R.layout.routeinfo); 
+        
+        tvDate = (TextView) findViewById(R.id.tvRouteDate);
+        tvTime = (TextView) findViewById(R.id.tvRouteDuration);
+        tvDistance = (TextView) findViewById(R.id.tvRouteDistance);
+        tvSpeed = (TextView) findViewById(R.id.tvRouteAvgSpeed);
+        
 	    //get info from intent
 	    Intent i = getIntent();
+	    routeKeyDB = i.getIntExtra("ROUTEKEY", 0);
+	    	   
+	    //get database info
+	    route = DatabaseHelper.GetRoute(routeKeyDB);
 	    
 	    //update screen
-	    
+	    tvDate.setText(route.getDate());
+	    tvTime.setText(route.getTime());
+	    tvDistance.setText(route.getDistance().toString());
 	}
 	
 	@Override
@@ -47,16 +67,17 @@ public class RouteInfoActivity extends Activity {
 	
 	public void doMapIt(View button)
 	{
-		ArrayList<MyGeoPoint> route = new ArrayList<MyGeoPoint>();
-		route.add(new MyGeoPoint(39313623,	-84282732));
-		route.add(new MyGeoPoint(39313847,	-84283859));
-		route.add(new MyGeoPoint(39314694,	-84284137));
-		route.add(new MyGeoPoint(39315831,	-84281509));
-		route.add(new MyGeoPoint(39318919,	-84279041));
-		route.add(new MyGeoPoint(39321500,	-84273033));
-		route.add(new MyGeoPoint(39319724,	-84271874));
-		route.add(new MyGeoPoint(39314188,	-84277571));
+//		ArrayList<MyGeoPoint> route = new ArrayList<MyGeoPoint>();
+//		route.add(new MyGeoPoint(39313623,	-84282732));
+//		route.add(new MyGeoPoint(39313847,	-84283859));
+//		route.add(new MyGeoPoint(39314694,	-84284137));
+//		route.add(new MyGeoPoint(39315831,	-84281509));
+//		route.add(new MyGeoPoint(39318919,	-84279041));
+//		route.add(new MyGeoPoint(39321500,	-84273033));
+//		route.add(new MyGeoPoint(39319724,	-84271874));
+//		route.add(new MyGeoPoint(39314188,	-84277571));
 		 
+		ArrayList<MyGeoPoint> route = MainActivity.DataBase.GetPoints(routeKeyDB);
 		Intent launchMap = new Intent(this, RunMapActivity.class);
 		launchMap.putExtra("Route", route);
 		startActivity(launchMap);	
@@ -108,10 +129,21 @@ public class RouteInfoActivity extends Activity {
 	public void doDeleteClick(View button)
 	{
 		//are you sure?
-		
-		//remove route from DB and return
-		
-		//return
-		finish();
+		new AlertDialog.Builder(this)
+		  .setTitle("DELETE?")
+		  .setMessage("Are you sure you want to delete this route?")
+		  .setPositiveButton("Yes", new DialogInterface.OnClickListener() 
+		  {
+				public void onClick(DialogInterface dialog, int whichButton) 
+				{
+					//remove route from DB and return
+					//DatabaseHelper.DeleteRoute(routeKeyDB)
+					
+					//return
+					finish();
+				}
+		  })
+		  .setNegativeButton("NO", null)
+		  .show();
 	}
 }
