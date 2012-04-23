e59032ccf8f8e55b9e15efcceb337b4281e28e7c
michael.clark.jr@gmail.com <michael.clark.jr@gmail.com@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-04-23 01:05:22 +0000

    Rmoved erroneos import

--- trunk/RunSpotRun/src/csc594/SemesterProject/DatabaseHelper.java
+++ trunk/RunSpotRun/src/csc594/SemesterProject/DatabaseHelper.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import csc594.SemesterProject.MainActivity.ItemBO;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.ContentValues;
