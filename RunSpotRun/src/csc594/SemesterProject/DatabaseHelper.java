871ca9556262d2b7ba936d16257fdec794c50194
michael.clark.jr <michael.clark.jr@gmail.com>
2012-04-23 06:02:00 +0000

    Filled in the methods, please watch return types and parameter types.

--- RunSpotRun/src/csc594/SemesterProject/DatabaseHelper.java
+++ RunSpotRun/src/csc594/SemesterProject/DatabaseHelper.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.google.android.maps.GeoPoint;
+
+import csc594.SemesterProject.MyGeoPoint.MyPointType;
+
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.ContentValues;
@@ -23,44 +28,161 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	@Override
 	public void onCreate(SQLiteDatabase db) 
 	{
-		db.execSQL("CREATE TABLE Route (_id INTEGER PRIMARY KEY ASC, Name TEXT, Date TEXT, StartTime TEXT, EndTime TEXT);");
-		db.execSQL("CREATE TABLE Point (_id INTEGER PRIMARY KEY ASC, FOREIGN KEY(RouteID) REFERENCES Route(_id), Latitude INTEGER, Longitude INTEGER, Time TEXT, Distance REAL);");
+		db.execSQL("CREATE TABLE Route (_id INTEGER PRIMARY KEY ASC, Name TEXT, Date TEXT, StartTime TEXT, EndTime TEXT, Distance TEXT);");
+		db.execSQL("CREATE TABLE Point (_id INTEGER PRIMARY KEY ASC, FOREIGN KEY(RouteID) REFERENCES Route(_id), Latitude INTEGER, Longitude INTEGER, Time TEXT, Distance TEXT);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) 
 	{
-		// TODO Auto-generated method stub
+		// TO DO Auto-generated method stub
 	}
 	
-	static ArrayList<RouteItem> GetRoutes()
+	ArrayList<RouteItem> GetRoutes()
 	{
 		//returns all routes
-		return new ArrayList<RouteItem>();
+		
+		ArrayList<RouteItem> routes = new ArrayList<RouteItem>();
+		
+		String SQL = "SELECT * FROM Route";
+        Cursor cur = this.getReadableDatabase().rawQuery(SQL, null);
+        
+        if (cur.moveToFirst())
+        {        
+	        while (!cur.isAfterLast())
+	        {	        	
+	        	routes.add(SetRouteFromCursor(cur));
+	        	
+	        	cur.moveToNext();
+	        }
+	        
+			return routes;
+        }
+        else
+        {	
+        	return new ArrayList<RouteItem>();
+        }	
 	}
 
-	static RouteItem GetRoute(int routeKey)
+	RouteItem GetRoute(int routeKey)
 	{
-		//return a single route for given route key
-		return new RouteItem();
-	}
+		//return a single route for given route key	
+		
+		String SQL = "SELECT * FROM Route WHERE _id = ?";
+        Cursor cur = this.getReadableDatabase().rawQuery(SQL, new String[] { Integer.toString(routeKey) });
+        
+        if (cur.moveToFirst())
+        {
+        	return SetRouteFromCursor(cur);
+        }
+        else
+        {
+        	return new RouteItem();
+        }
+    }
 
-	static ArrayList<MyGeoPoint> GetPoints(int routeKey)
+	ArrayList<MyGeoPoint> GetPoints(int routeKey)
 	{
 		//return all points for given route key
 		//<first>start, normal, <last>stop
 		
-		return new ArrayList<MyGeoPoint>();
+		ArrayList<MyGeoPoint> points = new ArrayList<MyGeoPoint>();
+		
+		String SQL = "SELECT * FROM Point WHERE RouteID = ?";
+        Cursor cur = this.getReadableDatabase().rawQuery(SQL, new String[] { Integer.toString(routeKey) });
+        int count = 0;
+        MyGeoPoint.MyPointType type;
+        
+        if (cur.moveToFirst())
+        {        
+	        while (!cur.isAfterLast())
+	        {
+	        	if (cur.isFirst())
+	        	{
+	        		type = MyGeoPoint.MyPointType.Start;
+	        	}
+	        	else if (cur.isLast())
+	        	{
+	        		type = MyGeoPoint.MyPointType.Stop;
+	        	}
+	        	else
+	        	{
+	        		type = MyGeoPoint.MyPointType.Normal;
+	        	}
+	        	
+	        	points.add(SetPointFromCursor(cur, "Point" + Integer.toString(count), type));
+	        	
+	        	cur.moveToNext();
+	        	count++;
+	        }
+	        
+			return points;
+        }
+        else
+        {	
+        	return new ArrayList<MyGeoPoint>();
+        }
 	}
 
-	static void AddAPoint(MyGeoPoint point, int routeKey)
+	long AddPoint(MyGeoPoint point, int routeKey)
 	{
 		//adds a point for given route key
+		
+		ContentValues cv = new ContentValues();
+		
+		cv.put("RouteID", routeKey);
+		cv.put("Latitude", point.getPoint().getLatitudeE6());
+		cv.put("Longitude", point.getPoint().getLongitudeE6());
+		cv.put("Time", point.getTime());
+		cv.put("Distance", point.getDistance());
+		return this.getWritableDatabase().insert("Point", "RouteID", cv);
+		
+		// TODO Make an UpdateRoute method, and add this distance to it's current distance
 	}
 
-	static int StartRoute(Date startTime)
+	long AddRoute(RouteItem route)
 	{
 		//adds a new route, returns the key
-		return 0;
+		
+		ContentValues cv = new ContentValues();
+		
+		cv.put("Name", route.getName());
+		cv.put("Date", route.getDate());
+		cv.put("StartTime", route.getTime());
+		cv.putNull("EndTime");
+		cv.put("Distance", 0);
+		return this.getWritableDatabase().insert("Point", "Name", cv);
+	}
+	
+	int DeleteRoute(int routeKey)
+	{
+		//deletes a single route for given route key
+		
+		return this.getWritableDatabase().delete("Route", "_id = ?", new String[] { Integer.toString(routeKey) });
+	}
+	
+	private RouteItem SetRouteFromCursor(Cursor cur)
+	{
+		RouteItem route = new RouteItem();
+		
+		route.setKey((int)cur.getLong(cur.getColumnIndex("_id")));
+    	route.setName(cur.getString(cur.getColumnIndex("Name")));
+    	route.setDate(cur.getString(cur.getColumnIndex("Date")));
+    	route.setTime(cur.getString(cur.getColumnIndex("StartTime")));
+    	route.setDistance((int)cur.getLong(cur.getColumnIndex("Distance"))); 
+    	
+    	return route;
+	}
+	
+	private MyGeoPoint SetPointFromCursor(Cursor cur, String name, MyPointType type)
+	{
+		int latitude = (int)cur.getLong(cur.getColumnIndex("Latitude"));
+		int longitude = (int)cur.getLong(cur.getColumnIndex("Longitude"));
+		String time = cur.getString(cur.getColumnIndex("Time"));
+		String distance = Double.toString(cur.getDouble(cur.getColumnIndex("Time")));
+		
+		MyGeoPoint point = new MyGeoPoint(latitude, longitude, time, distance, name, type);
+    	
+    	return point;
 	}
 }
