bedcc563050faeab9e4f1d76d4bf07ad7eb1b03e
johndirst <johndirst@343f6bd8-7ec5-7572-9d5d-b4f048550b3d>
2012-04-26 02:20:21 +0000

    updated meters to miles calc

--- trunk/RunSpotRun/src/csc594/SemesterProject/RunningService.java
+++ trunk/RunSpotRun/src/csc594/SemesterProject/RunningService.java
@@ -243,8 +243,10 @@ public class RunningService extends Service
 		try
 		{
 			float[] dist = new float[3];
-			Location.distanceBetween(p1.getLatitudeE6(), p1.getLongitudeE6(), p2.getLatitudeE6(), p2.getLongitudeE6(), dist);
-			return dist[0] * 0.000621371192;//meters to miles
+			//Location.distanceBetween(p1.getLatitudeE6(), p1.getLongitudeE6(), p2.getLatitudeE6(), p2.getLongitudeE6(), dist);
+			Location.distanceBetween(3932394,-84307652, 39324073,-84289284, dist);
+			
+			return dist[0] * 0.000621371192 / 5280;//meters to miles
 		}
 		catch(Exception e)
 		{
